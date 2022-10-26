c89a9933d410c87def351a2859982649d6d8c6be
SynysterRudro <rudrosingh82@gmail.com>
2022-10-26 14:01:57 +0600

    login page designed and register template created

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,39 +1,51 @@
+
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaGoogle, FaGithub } from "react-icons/fa";
 
 const Login = () => {
+    const handleSubmit = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.passoword.value;
+        console.log(email, password);
+    }
+
     return (
-        <div>
-            <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col lg:flex-row-reverse">
-                    <div className="text-center lg:text-left">
-                        <h1 className="text-5xl font-bold">Login now!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    </div>
-                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <div className="card-body">
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
-                                </label>
-                                <input type="text" placeholder="email" className="input input-bordered" />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
-                                <input type="text" placeholder="password" className="input input-bordered" />
-                                <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                                </label>
-                            </div>
-                            <div className="form-control mt-6">
-                                <button className="btn btn-primary">Login</button>
-                            </div>
+        <section className="p-6 dark:bg-gray-800 dark:text-gray-100">
+            <div className="container grid gap-6 mx-auto text-center lg:grid-cols-2 xl:grid-cols-5">
+                <div className="w-full px-6 py-16 rounded-md sm:px-12 md:px-16 xl:col-span-2 dark:bg-gray-900">
+                    <span className="block mb-2 dark:text-violet-400">Learn with pleasure</span>
+                    <h1 className="text-5xl font-extrabold dark:text-gray-50">CSE Raven</h1>
+                    <p className="my-8">
+                        SignIn First
+                    </p>
+                    <form onSubmit={handleSubmit} className="self-stretch space-y-3 ng-untouched ng-pristine ng-valid">
+
+                        <div>
+                            <label htmlFor="lastname" className="text-sm sr-only">Email address</label>
+                            <input type="text" name="email" className="w-full rounded-md focus:ring focus:ring-violet-400 p-2 dark:border-gray-700 text-black" placeholder='your email address' />
                         </div>
-                    </div>
+                        <div>
+                            <label htmlFor="lastname" className="text-sm sr-only">Your password</label>
+                            <input type="password" name="passoword" className="w-full rounded-md focus:ring focus:ring-violet-400 p-2 dark:border-gray-700 text-black" placeholder='your password' />
+                        </div>
+
+                        <button type="submit" className="w-full py-2 font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Sign In</button>
+                        <p>Need an account?<Link to='/register' className='text-blue-800 underline'>Register</Link> instead</p>
+                    </form>
                 </div>
+                <div>
+                    <p>Or login using</p>
+                    <button type="button" className="px-8 my-2 py-3 font-semibold border rounded hover:bg-slate-500 dark:border-gray-100 dark:text-gray-100"><FaGoogle></FaGoogle> Google</button>
+                    <br />
+                    <button type="button" className="px-8 py-3 font-semibold border rounded hover:bg-slate-500 dark:border-gray-100 dark:text-gray-100"><FaGithub></FaGithub>GitHub</button>
+
+                </div>
+
             </div>
-        </div>
+        </section>
     );
 };
 
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -3,7 +3,28 @@ import React from 'react';
 const Register = () => {
     return (
         <div>
-            <h1>I am register</h1>
+            <section className="p-6 dark:bg-gray-800 dark:text-gray-100">
+                <div className="container grid gap-6 mx-auto text-center lg:grid-cols-2 xl:grid-cols-5">
+                    <div className="w-full px-6 py-16 rounded-md sm:px-12 md:px-16 xl:col-span-2 dark:bg-gray-900">
+                        <span className="block mb-2 dark:text-violet-400">Learn with pleasure</span>
+                        <h1 className="text-5xl font-extrabold dark:text-gray-50">CSE Raven</h1>
+                        <p className="my-8">
+                            SignIn First
+                        </p>
+                        <form className="self-stretch space-y-3 ng-untouched ng-pristine ng-valid">
+                            <div>
+                                <label htmlFor="name" className="text-sm sr-only">Your name</label>
+                                <input id="name" type="text" placeholder="Your name" className="w-full rounded-md focus:ring focus:ring-violet-400 p-2 dark:border-gray-700" />
+                            </div>
+                            <div>
+                                <label htmlFor="lastname" className="text-sm sr-only">Email address</label>
+                                <input id="lastname" type="text" placeholder="Email address" className="w-full rounded-md focus:ring focus:ring-violet-400 p-2 dark:border-gray-700" />
+                            </div>
+                            <button type="button" className="w-full py-2 font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Sign In</button>
+                        </form>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
